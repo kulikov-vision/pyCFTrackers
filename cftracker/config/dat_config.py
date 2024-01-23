b7dccf92dd6fc87d75c46a118e05df72c97ff41f
kulikov-s-v <poison-black@yandex.ru>
2024-01-23 23:27:00 +0300

    small corrections (+ build)

--- cftracker/config/dat_config.py
+++ cftracker/config/dat_config.py
@@ -1,11 +1,11 @@
 import numpy as np
 class DATConfig:
-    img_scale_target_diagonal=75
+    img_scale_target_diagonal=60#75
     search_win_padding=2
     surr_win_factor=1.9
     color_space='rgb'
     num_bins=16
-    prob_lut_update_rate=0.05
+    prob_lut_update_rate=0.02# 0.05
     distractor_aware=True
     adapt_thresh_prob_bins=np.arange(0,1.001,0.05)
     motion_estimation_history_size=5
--- tracker_test.py
+++ tracker_test.py
@@ -2,14 +2,16 @@ import numpy as np
 import cv2
 import time
 
-from pyCFTrackers.cftracker.mosse import MOSSE
-from pyCFTrackers.cftracker.mkcfup import MKCFup
-from pyCFTrackers.cftracker.strcf import STRCF
-from pyCFTrackers.cftracker.config import staple_config,ldes_config,dsst_config,csrdcf_config,mkcf_up_config,mccth_staple_config
+from cftracker.mosse import MOSSE
+from cftracker.mkcfup import MKCFup
+from cftracker.strcf import STRCF
+from cftracker.dat import DAT
+from cftracker.config import staple_config,ldes_config,dsst_config,csrdcf_config,mkcf_up_config,mccth_staple_config
 
-cap = cv2.VideoCapture('../DATA/6230dc6a5e850c5cd7911308.mp4')
-tracker=MKCFup(config=mkcf_up_config.MKCFupLPConfig())
-#tracker=STRCF()
+cap = cv2.VideoCapture('/media/s-kulikov/44F2BB9AF2BB8F24/my-work/FPV/DATA/123.mp4')
+#tracker=MKCFup(config=mkcf_up_config.MKCFupLPConfig())
+tracker=DAT()
+
 
 left = top = -1
 w = 40
@@ -17,16 +19,21 @@ h = 30
 bbox = None
 def get_click(event,x,y,flags,param):
     global left, top, right, bottom, bbox
-    if event == cv2.EVENT_LBUTTONDOWN:        
+    if event == cv2.EVENT_LBUTTONDOWN:
         left, top = x, y            
         bbox = (left-w//2, top-h//2, w, h)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',get_click)
 
 ts = time.time()
+skip = 0
+i = 0
 while True:
+    i += 1
     ret, frame = cap.read()
     frame = cv2.resize(frame, (1280, 720))
+    if i < skip:
+        continue
     if not ret:
         break
         
@@ -41,7 +48,8 @@ while True:
 
     cv2.imshow('image', frame)
     delay = 34 - min(int(1000*(time.time() - ts)), 33)
+    print(1000*(time.time() - ts))
+
     ts = time.time()
-    print(delay)
     if cv2.waitKey(delay) == 27:
         bbox = None
