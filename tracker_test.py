406714bad166bf4362c27650b22fc7193ca71b69
kulikov-s-v <poison-black@yandex.ru>
2024-01-24 10:22:32 +0300

    choose optimal/fast trackers (Staple, MKCF, DAT)

--- tracker_test.py
+++ tracker_test.py
@@ -2,16 +2,29 @@ import numpy as np
 import cv2
 import time
 
+from lib.utils import get_img_list,get_ground_truthes,APCE,PSR
 from cftracker.mosse import MOSSE
+from cftracker.csk import CSK
+from cftracker.kcf import KCF
+from cftracker.cn import CN
+from cftracker.dsst import DSST
+from cftracker.staple import Staple
+from cftracker.dat import DAT
+from cftracker.eco import ECO
+from cftracker.bacf import BACF
+from cftracker.csrdcf import CSRDCF
+from cftracker.samf import SAMF
+from cftracker.ldes import LDES
 from cftracker.mkcfup import MKCFup
 from cftracker.strcf import STRCF
-from cftracker.dat import DAT
+from cftracker.mccth_staple import MCCTHStaple
+from lib.eco.config import otb_deep_config,otb_hc_config
 from cftracker.config import staple_config,ldes_config,dsst_config,csrdcf_config,mkcf_up_config,mccth_staple_config
 
 cap = cv2.VideoCapture('/media/s-kulikov/44F2BB9AF2BB8F24/my-work/FPV/DATA/123.mp4')
-#tracker=MKCFup(config=mkcf_up_config.MKCFupLPConfig())
-tracker=DAT()
-
+#tracker=DAT()  #SUPER FAST 300fps
+tracker=Staple(config=staple_config.StapleConfig()) #FAST 100fps
+#tracker=MKCFup(config=mkcf_up_config.MKCFupLPConfig()) #MAYBE   40-50 pfs
 
 left = top = -1
 w = 40
@@ -42,14 +55,15 @@ while True:
         tracker.init(frame, bbox)
         left = top = -1
     
+    ts0 = time.time()
     if bbox is not None:
         bbox = tracker.update(frame)
         cv2.rectangle(frame, np.array(bbox).astype(int), (0, 0, 255), 1)    
 
     cv2.imshow('image', frame)
-    delay = 34 - min(int(1000*(time.time() - ts)), 33)
-    print(1000*(time.time() - ts))
+    delay = 34 - min(int(1000*(time.time() - ts)), 33)    
 
     ts = time.time()
+    print(1000*(ts - ts0))
     if cv2.waitKey(delay) == 27:
         bbox = None
